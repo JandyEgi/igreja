202bfe66bc274d1a84b2b95b668e4d19793d26a0
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-06-21 12:07:14 +0100

    rename file

--- src/repository/RepositoryInterface.php
+++ src/repository/RepositoryInterface.php
@@ -1,8 +1,9 @@
 <?
 
-namespace Church\adapter;
+namespace Church\repository;
 
 interface RepositoryInterface{
+    public function index();
     public function created();
     public function updated();
     public function findAll(int $id);

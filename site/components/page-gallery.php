7ae7d6b5e486118f81024c086d50441774b3423b
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-06-21 12:14:00 +0100

    component gallery

---
+++ site/components/page-gallery.php
@@ -0,0 +1,9 @@
+<div class="row b-5">
+                <div class="col-md-4 my-img-card-style">
+                    <div class="hoverly">
+                        <a href="asess/img/imglight-1.jpg" class="img-link" data-lightbox="exemple-set" data-title="DevCode">
+                            <i class="fa fa-plus fa-5x text-white" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                    <img src="asess/img/imglight-1.jpg" class="img-fluid" alt="">
+                </div>

dbf23a3030cadf21825c13bd1d869981a0b4e618
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-06-21 12:15:25 +0100

    component menu site

---
+++ site/components/page-header.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white" id="menu">
+    <div class="container px-0">
+        <a class="navbar-brand d-flex align-items-center" href="#">
+            <div class="img-brand">
+                <img src="asess/icons/logo.svg" alt="">
+            </div>
+            <h5 class="mb-0 ml-2">CRISTIAN CHURCH</h5>
+        </a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0 pr-0">
+                <li class="nav-item item-style">
+                    <a href="/Church" class="nav-link">home</a>
+                </li>
+                <li class="nav-item item-style">
+                    <a class="nav-link" href="/Church/events">eventos</a>
+                </li>
+                <li class="nav-item item-style">
+                    <a class="nav-link" href="/Church/about">a igreja</a>
+                </li>
+                <li class="nav-item item-style">
+                    <a class="nav-link" href="/Church/gallery">galeria</a>
+                </li>
+                <li class="nav-item item-style">
+                    <a class="nav-link" href="/Church/about">sermões</a>
+                </li>
+                <li class="nav-item item-style">
+                    <a class="nav-link" href="blog.html">blog</a>
+                </li>
+                <li class="nav-item item-style">
+                    <a class="nav-link" href="#contact">contactos</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
